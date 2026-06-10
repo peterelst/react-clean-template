19348005b3f4d1d8f0ceec2e1b1ea24fba999e52
Peter Elst <peter.elst@gmail.com>
2026-06-10 22:55:47 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import { useEffect } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Route, Routes } from "react-router";
+import { AppLayout } from "./components/AppLayout";
+import { HomePage } from "./routes/HomePage";
+
+const queryClient = new QueryClient();
+
+export default function App() {
+  useEffect(() => {
+    document.title = "React Clean Template";
+  }, []);
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<AppLayout />}>
+            <Route path="/test" element={<HomePage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
+  );
+}
